e8191d04c70f2015b4a7484057933f9b16167d24
rsalvador22-2250 <martsalvador2018@gmail.com>
2026-06-05 17:25:00 +0800

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://tdhqodxptqzmkbfifmjl.supabase.co';
-const supabaseAnonKey = 'sb_publishable_ehZDndjyBFtODo-01cyVkg_Tte5C8Im';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
